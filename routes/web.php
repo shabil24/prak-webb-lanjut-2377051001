448b43dbc4edc4b2c104249497474020158f6161
unknown <200203526+shabil24@users.noreply.github.com>
2025-03-07 12:24:13 +0700

    menambahkan profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+
